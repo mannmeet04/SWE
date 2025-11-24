af74557be5cc53107c52442c548ca380ad16fdb7
Zakaria <zakaria.mouaouya@alumni.fh-aachen.de>
2025-11-24 14:14:37 +0100

    database

---
+++ code/dashboard/config/database.php
@@ -0,0 +1,22 @@
+<?php
+class Database {
+    private $host = "127.0.0.1";
+    private $user = "root";
+    private $pass = "1234";
+    private $dbname = "lernwebseite";
+    public $conn;
+
+    public function getConnection() {
+        try {
+            $dsn = "mysql:host=$this->host;dbname=$this->dbname;charset=utf8mb4";
+            $this->conn = new PDO($dsn, $this->user, $this->pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        } catch (PDOException $e) {
+            die("❌ Datenbankfehler: ".$e->getMessage());
+        }
+
+        return $this->conn;
+    }
+}
